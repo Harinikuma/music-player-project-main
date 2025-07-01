0a159244768eb6b86378a6529ca8c74fdfad2b92
Harini <harinikumar1325@gmail.com>
2025-07-01 15:16:09 +0530

    Fix: add react-scripts

--- react13/react13/src/api/music-palyer/services/music-palyer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * music-palyer service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::music-palyer.music-palyer');
